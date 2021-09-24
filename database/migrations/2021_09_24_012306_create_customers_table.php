5fd92a37d9f8368ea79e4d20c849881d2823461f
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-09-24 09:35:57 +0800

    created customer model and migration

---
+++ database/migrations/2021_09_24_012306_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string('id_customer', 15)->unique()->primary();
+            $table->string('nama_customer', 30);
+            $table->string('no_telpon', 13)->unique();
+            $table->string('alamat', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
