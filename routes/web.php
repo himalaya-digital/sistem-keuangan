1b90987ea3ed0a9ebd6a0cfb090ffa4da6faf968
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-10-16 21:36:24 +0800

    added laporan neraca

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Laporan\IndexController;
 use App\Http\Controllers\Laporan\LabaRugiController;
 use App\Http\Controllers\Laporan\PemasukanController;
 use App\Http\Controllers\Laporan\LaporanDataProyekController;
+use App\Http\Controllers\Laporan\LaporanNeracaController;
 use App\Http\Controllers\Laporan\PengeluaranController;
 use App\Http\Controllers\MasterData\DataAkunController;
 use App\Http\Controllers\MasterData\DataCustomerController;
@@ -99,6 +100,11 @@ Route::post('laporan-laba-rugi-export', [LabaRugiController::class, 'pdf'])->nam
 Route::get('laporan-arus-kas', [ArusKasController::class, 'index'])->name('laporan-arus-kas.index');
 Route::get('laporan-arus-kas-results', [ArusKasController::class, 'result'])->name('laporan-arus-kas.result');
 Route::post('laporan-arus-kas-export', [ArusKasController::class, 'pdf'])->name('lapran-arus-kas.pdf');
+
+Route::get('laporan-neraca', [LaporanNeracaController::class, 'index'])->name('laporan-neraca.index');
+Route::get('laporan-neraca/results', [LaporanNeracaController::class, 'results'])->name('laporan-neraca.results');
+Route::post('laporan-neraca/export', [LaporanNeracaController::class, 'export'])->name('laporan-neraca.export');
+
 
 Route::prefix('laporan')->group(function () {
     Route::get('data-proyek', [LaporanDataProyekController::class, 'index'])->name('laporan-data-proyek.index');
